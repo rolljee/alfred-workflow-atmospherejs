bd6b2b082c062b86049ee7b708d9a7b4eb8c5fea
Rolljee <n.riquelmebareiro@gmail.com>
2017-10-25 20:04:31 +0200

    Started working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+alfy.fetch('https://atmospherejs.com/?q=modal').then(data => {
+	const items = alfy
+		.inputMatches(data, 'title')
+		.map(x => ({
+			title: x.title,
+			subtitle: x.body,
+			arg: x.id
+		}));
+
+	alfy.output(items);
+});
